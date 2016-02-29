4cf428ef37cfcfee218ce6da868aaba743449cc8
Wei-Jen <weijen0330@hotmail.com>
2016-02-29 02:48:01 -0800

    updated works and contact

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 /**
  * Created by Wei-Jen on 2/28/16.
  */
+
+$(function() {
+    $('#fullpage').fullpage({
+        anchors: ['about', 'works', 'contacts'],
+        menu: '#myMenu'
+    });
+    $('.Collage').collagePlus();
+});
